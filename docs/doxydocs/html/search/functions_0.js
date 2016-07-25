9972a9fe5ffb6566b68ddd4cf3223ea016a9f165
David York <david@debian2x8david>
2016-07-25 18:31:25 -0400

    updated doxygen docs

---
+++ docs/doxydocs/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['getdatastruct',['getDataStruct',['../class_util_c_s_v.html#a61cd010fac94469c7b71ddd4705407e1',1,'UtilCSV']]],
+  ['getncols',['getNcols',['../class_util_c_s_v.html#a6bbaaae883fdc9b7c92c9ca2772d7b25',1,'UtilCSV']]]
+];
---
+++ docs/doxydocs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['utilcvs',['UtilCVS',['../md__r_e_a_d_m_e.html',1,'']]]
+];
